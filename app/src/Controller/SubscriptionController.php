7f9f1c19218ebb9fb8b6867e5bf644fa69327365
root <root@LAPTOP-L2B-HA.localdomain>
2023-01-25 17:57:30 +0000

    commit for ecommerce

--- app/src/Controller/SubscriptionController.php
+++ app/src/Controller/SubscriptionController.php
@@ -42,6 +42,7 @@ class SubscriptionController extends AbstractController
         $amount = $this->getSubscriptionFee($category);
         $payment_redirect_url = $this->payForSubscription($amount, $user);
 
+        dump($payment_redirect_url);
         if($payment_redirect_url) return $this->redirect($payment_redirect_url);
         else return $this->redirectToRoute('subscription_payment_category');
 
